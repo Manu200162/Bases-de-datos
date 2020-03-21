368fda841f8097e779f33feec6cfc4054a3c47da
Manu200162 <manli200134@gmail.com>
2020-03-20 20:28:14 -0400

    no message

--- clase_java/Main.java
+++ clase_java/Main.java
@@ -5,23 +5,30 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Conector instancia = Conector.getInstancia();
-		//try {
-			//instancia.getNombres();
-			//for (String s : instancia.getNombres()) {
-			//	System.out.println(s);
-			//}
+		/*
+		 * 3) Mostrar los nombres de los pacientes.
+		 */
+		try {
+			System.out.println("Nombres");
+			for (String s : instancia.getNombres()) {
+				System.out.println(s);
+			}
 
-		//} catch (SQLException e) {
-		//	e.printStackTrace();
-		//}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("");
+		System.out.println("");
+		/*
+		 * 4) Mostrar el consultorio y nombre de los doctores
+		 */
 		try {
 			System.out.println("DOCTOR    CONSULTORIO");
-			for (int i = 0; i< instancia.getDoctores().size(); i++) {
-				System.out.print(instancia.getDoctores().get(i));  
-			    i++;
-			    System.out.println("      "+instancia.getDoctores().get(i));  
+			for (int i = 0; i < instancia.getDoctores().size(); i++) {
+				System.out.print(instancia.getDoctores().get(i));
+				i++;
+				System.out.println("      " + instancia.getDoctores().get(i));
 			}
 
 		} catch (SQLException e) {
